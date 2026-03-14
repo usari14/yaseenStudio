f0450e411b4470cd7e72ac31047da4755e0589ed
usari14 <udogar583@gmail.com>
2026-03-15 01:45:59 +0500

    feat: Add GitHub Pages deployment workflow and configure Next.js for static export.

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,22 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+let basePath = "";
+
+if (isGithubActions) {
+  // e.g. "usari14/yaseenStudio" -> "yaseenStudio"
+  const repo = process.env.GITHUB_REPOSITORY?.replace(/.*?\//, "");
+  if (repo) {
+    basePath = `/${repo}`;
+  }
+}
+
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "export",
+  basePath: basePath !== "" ? basePath : undefined,
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
